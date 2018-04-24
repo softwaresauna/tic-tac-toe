823493f170981ed4ab6935fe7dcfb16da82b77d0
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-24 12:58:22 +0200

    added import, improved board helper api

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,10 +1,11 @@
 import {alternateTurn} from "./turn-alternator";
+import {isFinished} from "./finish-resolver";
 
 export function move(gameState, row, column) {
 
     console.log("move: ", gameState, "cell: " + row + ", " + column);
 
-    const nextBoard = putPieceOnBoard(gameState, row, column);
+    const nextBoard = putPieceOnBoard(gameState.board, gameState.turn);
     const winner = calculateWinner(nextBoard);
 
     const nextGameState = {};
