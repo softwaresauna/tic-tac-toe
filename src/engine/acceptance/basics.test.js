513a91dd413782e12db6dae72a58d6192307dbdd
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-24 10:42:25 +0200

    removed turn alternation acceptance test (covered by unit test)

--- src/engine/acceptance/basics.test.js
+++ src/engine/acceptance/basics.test.js
@@ -1,18 +1,7 @@
 import {move} from "../engine";
 import {createInitialState, O, X} from "../state";
 
-test('turn alternates', () => {
 
-    const state1 = move(createInitialState(), 0, 0);
-    const state2 = move(state1, 0, 1);
-    const state3 = move(state2, 1, 0);
-    const state4 = move(state3, 1, 1);
-
-    expect(state1.turn).toBe(X);
-    expect(state2.turn).toBe(O);
-    expect(state3.turn).toBe(X);
-    expect(state4.turn).toBe(O);
-});
 
 test('not finished during game', () => {
 
