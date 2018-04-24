28aa8332e83dd1f4b8cd4772faf76215d549ee2d
mbjelac <marko.bjelac@softwaresauna.com>
2018-04-24 10:41:33 +0200

    implemented move interactor

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,4 +1,17 @@
+import {alternateTurn} from "./turn-alternator";
+
 export function move(gameState, row, column) {
 
     console.log("move: ", gameState, "cell: " + row + ", " + column);
+
+    const nextBoard = putPieceOnBoard(gameState, row, column);
+    const winner = calculateWinner(nextBoard);
+
+    const nextGameState = {};
+    nextGameState.board = nextBoard;
+    nextGameState.turn = alternateTurn(gameState.turn);
+    nextGameState.winner = winner;
+    nextGameState.finished = isFinished(winner, nextBoard);
+
+    return nextGameState;
 }
