e89f138070fd905a01df3749ae09c14c1b08f8c0
mbjelac <marko.bjelac@softwaresauna.com>
2018-06-13 10:18:54 +0200

    added cell click event. extracted cell content delivery to method

--- src/gui/Cell.js
+++ src/gui/Cell.js
@@ -6,6 +6,16 @@ export class Cell extends StateObserver {
 
     render() {
 
-        return <td className='Cell'>{this.state.board[this.props.row][this.props.column]}</td>
+        return <td
+            className='Cell'
+            onClick={this.onClick}>
+            {this.getContents()}
+        </td>
     }
+
+    onClick = () => {
+        console.info(`Clicked cell (${this.props.row}, ${this.props.column})!`);
+    };
+
+    getContents = () => this.state.board[this.props.row][this.props.column];
 }
